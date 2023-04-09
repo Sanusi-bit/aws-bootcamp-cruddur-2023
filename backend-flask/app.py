261215dfdd4d4f4e0881f21dded4183ffb8cff4a
Sanusi Waris <sanusiadeniyi1@gmail.com>
2023-04-09 12:22:31 +0000

    Integrating jwttoken verification from server side

--- backend-flask/app.py
+++ backend-flask/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import request
 from flask_cors import CORS, cross_origin
 import os
+import sys
 
 from services.home_activities import *
 from services.notifications_activities import *
@@ -13,6 +14,8 @@ from services.message_groups import *
 from services.messages import *
 from services.create_message import *
 from services.show_activity import *
+
+from lib.cognito_token_verification import CognitoTokenVerification
 
 # Honeycomb Initialization
 from opentelemetry import trace
@@ -55,6 +58,12 @@ tracer = trace.get_tracer(__name__)
 
 app = Flask(__name__)
 
+cognito_token_verification = CognitoTokenVerification(
+  user_pool_id= os.getenv("AWS_COGNITO_USER_POOL_ID"),
+  user_pool_client_id= os.getenv("AWS_COGNITO_USER_POOL_CLIENT_ID"),
+  region= os.getenv("AWS_DEFAULT_REGION")
+)
+
 # Initializing rollbar
 import os
 import rollbar
@@ -76,8 +85,8 @@ origins = [frontend, backend]
 cors = CORS(
   app, 
   resources={r"/api/*": {"origins": origins}},
-  expose_headers="location,link",
-  allow_headers="content-type,if-modified-since",
+  headers=['Content-Type', 'Authorization'], 
+  expose_headers='Authorization',
   methods="OPTIONS,GET,HEAD,POST"
 )
 
@@ -147,7 +156,14 @@ def data_create_message():
 @app.route("/api/activities/home", methods=['GET'])
 @xray_recorder.capture('activities_home')
 def data_home():
+  app.logger.debug("AUTH HEADER")
+  app.logger.debug(
+    request.headers.get('Authorization')
+  )
   data = HomeActivities.run(logger=LOGGER)#logger= LOGGER
+  claims = aws_auth.claims # also available through g.cognito_claims
+  app.logger.debug('claims')
+  app.logger.debug(claims)
   return data, 200
 
 @app.route("/api/activities/notifications", methods=['GET'])
